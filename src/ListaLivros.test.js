69881d4fb27222258e6e762f1cef4ad72614e04d
alysson.morais <alysson.morais@dataprev.gov.br>
2025-04-18 16:06:16 -0300

    cap13 - initial settings

--- src/ListaLivros.test.js
+++ src/ListaLivros.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import ListaLivros from './ListaLivros';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<ListaLivros />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
