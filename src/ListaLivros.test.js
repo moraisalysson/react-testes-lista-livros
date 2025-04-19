0b055335aabfa15f3ad8a50594c8e2cb785bc7a1
alysson.morais <alysson.morais@dataprev.gov.br>
2025-04-18 21:27:12 -0300

    cap13 - testes com strings

--- src/ListaLivros.test.js
+++ src/ListaLivros.test.js
@@ -1,8 +1,47 @@
 import { render, screen } from '@testing-library/react';
 import ListaLivros from './ListaLivros';
 
-test('renders learn react link', () => {
+//"it" substitui o termo "test"
+// it('renders learn react link', () => {
+//   render(<ListaLivros />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+it('Verify if save to reload exists', () => {
   render(<ListaLivros />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const string = screen.getByText(/save to reload/);
+  expect(string).toBeInTheDocument();
 });
+
+it('verifica a substring e ignora case', () => {
+  render(<ListaLivros />);
+  const string = screen.getByText(/o reload/i);
+  expect(string).toBeInTheDocument();
+});
+
+
+// test('verify strings', () => {
+//   render(<ListaLivros />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   const string = screen.getByText(/save to reload/);
+  
+//   expect(linkElement).toBeInTheDocument();
+//   expect(string).toBeInTheDocument();
+// });
+
+// describe("Teste de soma", () => {
+//   function soma(a, b) {
+//     return a + b;
+//   }
+
+//   test("O resultado deve ser 5", () => {
+//     expect(soma(2, 3)).toBe(5);
+// }))}
+
+// describe("Teste de presença de item em array", () => {
+//   const NOMES = ["Maurício", "Maujor", "Samy", "Silva"]
+//   test("Testa a presença do item 'Maujor no array NOMES", () => {
+//     expect(NOMES).toContain("Maujor");
+//   })
+// })
